59e71bfd90e4e9c80dd5fd8e5196c3aab7dfadac
avi20298patel <avi20298patel@gmail.com>
2023-11-01 00:21:58 +0530

    carousel with arrow button

--- src/screens/IntroDuction.js
+++ src/screens/IntroDuction.js
@@ -1,111 +1,71 @@
-import { View, Text, TextInput, Image, Dimensions } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { StatusBar } from 'expo-status-bar'
+import { View, Text, Image } from 'react-native'
+import React, { useState } from 'react'
 import manFull from '../../assets/images/man-full.png'
 // import * as Icon from 'react-native-feather'
-import Carousel from 'react-native-reanimated-carousel';
+import Slick from 'react-native-slick';
 
 const IntroDuction = () => {
-    const width = Dimensions.get('window').width;
+    const [page, setPage] = useState(0)
+    const handlePage = (value, total) => {
+        setPage(value)
+        if (value == total) {
+            console.log('true')
+        } else {
+            console.log('false')
+        }
+    }
     return (
-        <SafeAreaView className="bg-white">
-            <StatusBar barStyle="dark-content " />
-            <Carousel
-                loop
-                width={width}
-                // height={width}
-                autoPlay={true}
-                data={[...new Array(6).keys()]}
-                scrollAnimationDuration={500}
-                mode="parallax"
-                modeConfig={{
-                    parallaxScrollingScale: 0.9,
-                    parallaxScrollingOffset: 50,
-                }}
-                // onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View className="item text-center ">
-                        <Image
-                            className="rounded-2xl"
-                            source={manFull}
-                        />
 
-                        <Text className="text-primary my-4  block text-4xl">
-                            <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
-                            <Text className="uppercase italic font-bold"> Technician?</Text>
-                        </Text>
+        <Slick showsButtons={true} renderPagination={(index, total) => handlePage(index + 1, total)}>
+            <View className="item text-center ">
+                <Image
+                    className="rounded-2xl"
+                    source={manFull}
+                />
 
-                        <Text className="text-neutral-600 font-thin">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        </Text>
-                    </View>
-                )}
-            />
-            {/* <View className="owl-carousel p-4  ">
-                <View className="item text-center ">
-                    <Image
-                        className="rounded-2xl"
-                        source={manFull}
-                    />
+                <Text className="text-primary my-4  block text-4xl">
+                    <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
+                    <Text className="uppercase italic font-bold"> Technician?</Text>
+                </Text>
 
-                    <Text className="text-primary my-4  block text-4xl">
-                        <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
-                        <Text className="uppercase italic font-bold"> Technician?</Text>
-                    </Text>
+                <Text className=" text-red-600 font-bold">
+                    {page}
+                </Text>
+            </View>
+            <View className="item text-center ">
+                <Image
+                    className="rounded-2xl"
+                    source={manFull}
+                />
 
-                    <Text className="text-neutral-600 font-thin">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    </Text>
-                </View>
-                <View className="item text-center ">
-                    <Image
-                        className="rounded-2xl"
-                        source={manFull}
-                    />
+                <Text className="text-primary my-4  block text-4xl">
+                    <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
+                    <Text className="uppercase italic font-bold"> Technician?</Text>
+                </Text>
 
-                    <Text className="text-primary my-4  block text-4xl">
-                        <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
-                        <Text className="uppercase italic font-bold"> Technician?</Text>
-                    </Text>
+                <Text className=" text-red-600 font-bold">
+                    {page}
+                </Text>
+            </View>
+            <View className="item text-center ">
+                <Image
+                    className="rounded-2xl"
+                    source={manFull}
+                />
 
-                    <Text className="text-neutral-600 font-thin">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    </Text>
-                </View>
-                <View className="item text-center ">
-                    <Image
-                        className="rounded-2xl"
-                        source={manFull}
-                    />
+                <Text className="text-primary my-4  block text-4xl">
+                    <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
+                    <Text className="uppercase italic font-bold"> Technician?</Text>
+                </Text>
 
-                    <Text className="text-primary my-4  block text-4xl">
-                        <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
-                        <Text className="uppercase italic font-bold"> Technician?</Text>
-                    </Text>
-
-                    <Text className="text-neutral-600 font-thin">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    </Text>
-                </View>
-                <View className="item text-center ">
-                    <Image
-                        className="rounded-2xl"
-                        source={manFull}
-                    />
-
-                    <Text className="text-primary my-4  block text-4xl">
-                        <Text className="uppercase space-x-3 font-extralight tracking-widest">Are You a</Text>
-                        <Text className="uppercase italic font-bold"> Technician?</Text>
-                    </Text>
-
-                    <Text className="text-neutral-600 font-thin">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    </Text>
-                </View>
-            </View> */}
-        </SafeAreaView>
+                <Text className=" text-red-600 font-bold">
+                    {page}
+                </Text>
+            </View>
+        </Slick>
     )
 }
 
 export default IntroDuction
+
+
