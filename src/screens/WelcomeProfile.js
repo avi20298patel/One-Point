c38f35a3fb84d70935d753ae5360b959276384b2
avi20298patel <avi20298patel@gmail.com>
2023-11-04 18:16:26 +0530

    pr for staging deployment

--- src/screens/WelcomeProfile.js
+++ src/screens/WelcomeProfile.js
@@ -5,7 +5,7 @@ import DynamicButton from '../components/Global/DynamicButton'
 export default function WelcomeProfile({ navigation }) {
     return (
         <View className="flex flex-col justify-between content-between  p-6  h-screen w-full">
-            <Text className="text-2xl text-center mt-20">Welcome Profile</Text>
+            <Text className="text-2xl text-center mt-20">Welcome Profile 1</Text>
             <DynamicButton
                 btnText="Next"
                 navigation={navigation}
