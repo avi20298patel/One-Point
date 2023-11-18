f498c05f99840c608e81f672a5b9f4e6396b7581
avi20298patel <avi20298patel@gmail.com>
2023-11-18 19:32:48 +0530

    added react native gesture handler at top of app.js

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native';
 import Navigation from './src/navigation';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 3 }}>
-      <StatusBar
-        backgroundColor={'#fff'}
-        barStyle={'dark-content'}
-        className="p-2"
-      />
-      <Navigation />
-    </SafeAreaView>
-    // <Navigation />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 3 }}>
+        <StatusBar
+          backgroundColor={'#fff'}
+          barStyle={'dark-content'}
+          className="p-2"
+        />
+        <Navigation />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
